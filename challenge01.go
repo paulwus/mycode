418dc5c8bcae6286d41e71e6135f618e0de36683
paulwus <paulwus@users.noreply.github.com>
2022-06-13 15:57:26 +0000

    variables

---
+++ challenge01.go
@@ -0,0 +1,14 @@
+/* Alta3 Research | RZFeeser
+   Exported names   */
+   
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Paul Watt")  
+    fmt.Println("Mahtomedi, MN")
+}
+
